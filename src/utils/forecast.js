18112239f0da5d6ee79c7096a246d8ca2eb27125
yehuda92 <92516903+yehuda92@users.noreply.github.com>
2021-12-23 10:24:35 +0200

    Delete the tests messages

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,5 @@
 const request = require('request')
-const testMsg = "This msg was created to practis on github and heroku" 
-const testMsgTwo = "This msg was created to practis on github and heroku" 
+
 
 
 const forecast = (latitude, longitude, callback) => {
@@ -10,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         if(err) {
             callback('Unable to conect to Weather service!', undefined)
         } else if(body.error) {
-            callback('Unable to find location!' +  testMsg + testMsgTwo, undefined)
+            callback('Unable to find location!', undefined)
         } else {
             callback(undefined, {
                 current: body.current.weather_descriptions,
