6f7079eb23f4a12d8cfef8da1fec0eecf471d55d
yehuda92 <92516903+yehuda92@users.noreply.github.com>
2021-12-23 10:19:27 +0200

    Add some tests messages

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,7 @@
 const request = require('request')
+const testMsg = "This msg was created to practis on github and heroku" 
+const testMsgTwo = "This msg was created to practis on github and heroku" 
+
 
 const forecast = (latitude, longitude, callback) => {
    const url = 'http://api.weatherstack.com/current?access_key=e372ce6dfa3243a66ca49e8cd1c7a062&query='+ latitude + ',' + longitude +'&unit=f'
@@ -7,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         if(err) {
             callback('Unable to conect to Weather service!', undefined)
         } else if(body.error) {
-            callback('Unable to find location!', undefined)
+            callback('Unable to find location!' +  testMsg + testMsgTwo, undefined)
         } else {
             callback(undefined, {
                 current: body.current.weather_descriptions,
